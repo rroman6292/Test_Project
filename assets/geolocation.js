01f1c4df3bf6ee248d2b9cf8adcd8c2b01f043aa
Richie Roman <answer417@gmail.com>
2021-02-02 13:33:06 -0500

    add geo

--- assets/geolocation.js
+++ assets/geolocation.js
@@ -1,31 +1,44 @@
-function geoFindMe() {
-
-  const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
-
-  mapLink.href = '';
-  mapLink.textContent = '';
-
-  function success(position) {
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    status.textContent = '';
-    mapLink.href = "https://www.openstreetmap.org/#map=18/${latitude}/${longitude}";
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-  }
-
-  function error() {
-    status.textContent = 'Unable to retrieve your location';
-  }
-
-  if(!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
+function showPosition() {
+  if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showMap, showError);
   } else {
-    status.textContent = 'Locating…';
-    navigator.geolocation.getCurrentPosition(success, error);
+      alert("Sorry, your browser does not support HTML5 geolocation.");
   }
-
 }
 
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
+function showMap(position) {
+
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  var latlong = new google.maps.LatLng(lat, long);
+  
+  var myOptions = {
+      center: latlong,
+      zoom: 16,
+      mapTypeControl: true,
+      navigationControlOptions: {
+          style:google.maps.NavigationControlStyle.SMALL
+      }
+  }
+  
+  var map = new google.maps.Map(document.getElementById("embedMap"), myOptions);
+  var marker = new google.maps.Marker({ position:latlong, map:map, title:"You are here!" });
+}
+
+function showError(error) {
+  if(error.code == 1) {
+      result.innerHTML = "You've decided not to share your position, but it's OK. We won't ask you again.";
+  } else if(error.code == 2) {
+      result.innerHTML = "The network is down or the positioning service can't be reached.";
+  } else if(error.code == 3) {
+      result.innerHTML = "The attempt timed out before it could get the location data.";
+  } else {
+      result.innerHTML = "Geolocation failed due to unknown error.";
+  }
+}
+
+
+
+document.getElementById("useMyLoc").onclick = showPosition();
+
+
